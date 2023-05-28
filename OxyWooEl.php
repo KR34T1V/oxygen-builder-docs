5c8fd726a002cdbcc7d92c1a4b8a07afb249380b
Cornelis Terblanche <1997corry@gmail.com>
2023-05-28 19:23:10 +0200

    Update OxyWooEl.php

--- OxyWooEl.php
+++ OxyWooEl.php
@@ -1,69 +1,61 @@
 <?php
 /**
- * Class OxyWooEl
- *
- * This class extends the OxyEl class and provides additional functionality related to WooCommerce.
+ * Represents the OxyWooEl class, which extends the OxyEl class.
  */
 class OxyWooEl extends OxyEl {
     /**
-     * Initialization method
-     *
-     * This method initializes the class by setting the assets path and enabling AJAX controls.
+     * Initializes the Oxygen WooCommerce Element.
+     * This method enables AJAX controls for the element and sets the assets path.
      */
     function init() {
         $this->El->useAJAXControls();
-        $this->setAssetsPath( OXY_WOO_ASSETS_PATH );
+        $this->setAssetsPath(OXY_WOO_ASSETS_PATH);
     }
 
     /**
-     * Render method
+     * Renders the Oxygen WooCommerce Element.
      *
-     * This method is responsible for rendering the element based on the provided options, defaults, and content.
-     *
-     * @param array $options The options for rendering the element.
-     * @param array $defaults The default values for the element options.
-     * @param string $content The content to be rendered within the element.
+     * @param array $options - The options for rendering.
+     * @param array $defaults - The default options.
+     * @param string $content - The content to be rendered.
      */
     function render($options, $defaults, $content) {
+        // Check if the 'wooTemplate' method exists in the class
         if (method_exists($this, 'wooTemplate')) {
+            // Access the global $product variable
             global $product;
             $product = wc_get_product();
 
+            // If a product exists, call the 'wooTemplate' method
             if ($product != false) {
-                // Call the wooTemplate method defined in the extending class
                 call_user_func($this->wooTemplate());
             }
         }
     }
 
     /**
-     * Get class names
+     * Defines the class names for the Oxygen WooCommerce Element.
      *
-     * This method returns an array of class names associated with the OxyWooEl element.
-     *
-     * @return array An array of class names.
+     * @return array - An array of class names.
      */
     function class_names() {
         return array('oxy-woo-element');
     }
 
     /**
-     * Get WooCommerce button place
+     * Defines the button place for the Oxygen WooCommerce Element.
      *
-     * This method returns the position where the WooCommerce button should be placed.
-     *
-     * @return string The button place value.
+     * @return string - The button place.
      */
     function woo_button_place() {
         return "other";
     }
 
     /**
-     * Get button place
+     * Gets the button place for the Oxygen WooCommerce Element.
+     * This method returns the button place in the format 'woo::button_place'.
      *
-     * This method determines the final button place value for the element.
-     *
-     * @return string The final button place value.
+     * @return string - The button place.
      */
     function button_place() {
         $woo_button_place = $this->woo_button_place();
@@ -75,3 +67,11 @@ class OxyWooEl extends OxyEl {
         return "";
     }
 }
+
+// The provided code represents a PHP class called OxyWooEl which extends the OxyEl class. Here are the details of the class and its methods:
+
+// init(): This method initializes the Oxygen WooCommerce Element. It enables AJAX controls for the element and sets the assets path.
+// render($options, $defaults, $content): This method renders the Oxygen WooCommerce Element. It checks if the 'wooTemplate' method exists in the class, accesses the global $product variable, and calls the 'wooTemplate' method if a product exists.
+// class_names(): This method defines the class names for the Oxygen WooCommerce Element and returns them as an array.
+// woo_button_place(): This method defines the button place for the Oxygen WooCommerce Element and returns it as a string.
+// button_place(): This method gets the button place for the Oxygen WooCommerce Element. It calls the woo_button_place() method and returns the button place in the format 'woo::button_place'.
